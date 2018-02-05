2265ac1ce6b749e32cb9f64d7222218462c95ae2
migrow <michael.growette@gmail.com>
2018-02-05 14:15:28 -0500

    GET /cats

--- api.js
+++ api.js
@@ -6,6 +6,15 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
 const {} = require('ramda')
 
+const cats = [
+  { name: 'Mittens', age: 3, gender: 'Female', breed: 'Tabby' },
+  { name: 'Muffins', age: 2, gender: 'Male', breed: 'Siamese' },
+  { name: 'Mr. Handsome', age: 8, gender: 'Male', breed: 'Tom' },
+  { name: 'Miss Krunkles', age: 5, gender: 'Female', breed: 'Tabby' }
+]
+
 app.get('/', (req, res) => res.send('Meow.'))
 
-app.listen(port, () => console.log('CATS!', port))
+app.get('/cats', (req, res) => res.send(cats))
+
+app.listen(port, () => console.log(port, 'CATS! OH MY!'))
